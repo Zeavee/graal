622a14c05cb83fb9ffe494e06caf08a50e73c757
David Kozak <david.kozak@oracle.com>
2022-02-16 09:57:38 +0100

    finally fix the interplay of reachability analysis and heap snapshotting

--- substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/PointsToAnalysis.java
+++ substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/PointsToAnalysis.java
@@ -653,11 +653,6 @@ public abstract class PointsToAnalysis implements BigBang {
         field.registerAsAccessed();
     }
 
-    @Override
-    public void markMethodImplementationInvoked(AnalysisMethod method, Object reason) {
-        addRootMethod(method).registerAsImplementationInvoked();
-
-    }
 
     @SuppressWarnings("try")
     public boolean doTypeflow() throws InterruptedException {
